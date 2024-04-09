3fba9c61990913e9c3a5275aabbdf92aded15d59
kairveeehh <jampu741@gmail.com>
2024-04-09 23:43:16 +0530

    changed assets , yoga feature and breastfeed

--- bfeed.js
+++ bfeed.js
@@ -5,7 +5,7 @@ document.getElementById("showGuide").addEventListener("click", function() {
   } else {
       guideContent.style.display = "block";
       // You can populate guide content dynamically here
-      guideContent.innerHTML = "<p>Here are some tips for successful breastfeeding:</p><ul><li>Ensure proper latching</li><li>Feed frequently, on demand</li><li>Stay hydrated</li><li>Seek help from a lactation consultant if needed</li><li>Thrush: Fungal infection causing white patches in the baby's mouth and nipple pain in the mother.</li><li>Engorged Breasts: Overfilled breasts causing swelling, pain, and difficulty latching for the baby.</li><li>Plugged Duct: Blockage in a milk duct leading to localized pain and inflammation.</li><li>Breast Abscess: Collection of pus in the breast tissue due to bacterial infection.</li><li>Nipple Trauma: Injuries or damage to the nipple tissue resulting from improper latching or sucking.</li></ul>";
+      guideContent.innerHTML = "<p>Here are some tips for successful breastfeeding:</p><li>Ensure proper latching</li><li>Feed frequently, on demand</li><li>Stay hydrated</li><li>Seek help from a lactation consultant if needed</li><li>Thrush: Fungal infection causing white patches in the baby's mouth and nipple pain in the mother.</li><li>Engorged Breasts: Overfilled breasts causing swelling, pain, and difficulty latching for the baby.</li><li>Plugged Duct: Blockage in a milk duct leading to localized pain and inflammation.</li><li>Breast Abscess: Collection of pus in the breast tissue due to bacterial infection.</li><li>Nipple Trauma: Injuries or damage to the nipple tissue resulting from improper latching or sucking.</li>";
   }
 });
 
@@ -16,6 +16,6 @@ document.getElementById("showSymptoms").addEventListener("click", function() {
   } else {
       symptomsContent.style.display = "block";
       // You can populate symptoms content dynamically here
-      symptomsContent.innerHTML = "<p>Here are some common riskful symptoms during breastfeeding:</p><ul><li>Sore or cracked nipples</li><li>Engorged breasts</li><li>Low milk supply</li><li>Mastitis (breast infection)</li><li>Thrush (yeast infection in the breast)</li></ul>";
+      symptomsContent.innerHTML = "<p>Here are some common riskful symptoms during breastfeeding:</p><li>Sore or cracked nipples</li><li>Engorged breasts</li><li>Low milk supply</li><li>Mastitis (breast infection)</li><li>Thrush (yeast infection in the breast)</li>";
   }
 });
